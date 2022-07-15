faea77560aad1e67a199ba3463ed7f8f483ce26c
Hiago Duarte <hiagofdss900@gmail.com>
2022-07-15 19:37:07 -0300

    cod_001

---
+++ cd_001.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Text;
+using System.Collections;
+using System.Linq;
+using System.Threading;
+using System.Net;
+
+
+namespace HelloWord 
+{
+    class Program
+    {
+        static void Main(string[ ] args)
+        {
+              Console.write("Hello Word");
+
+
+        }
+    }
+    
+
+}
+
