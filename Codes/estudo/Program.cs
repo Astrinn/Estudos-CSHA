1e0612ecc7b8bfb6e52637d5bd281945ed4e140f
Hiago Duarte <hiagofdss900@gmail.com>
2022-07-29 18:56:45 -0300

    att-018

--- Codes/estudo/Program.cs
+++ Codes/estudo/Program.cs
@@ -10,18 +10,23 @@ namespace estudo
 {
     public class Program
     {
-        enum Opcao { Criar = 1, Deletar = 2, Editar = 3, Listar = 4, Atualizar = 5 };
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Selecione uma das opções abaixo:");
-            Console.WriteLine("1-Criar\n2-Deletar\n3-Editar\n4-Listar\n5-Atualizar");
-            int index = int.Parse(Console.ReadLine());
-            Opcao OpcaoSelecionada = (Opcao)index;
+           int contador = 0;
+            while (contador < 10)
+            {
+                Console.WriteLine(contador + 1);
+                Console.WriteLine("while está sendo executado");
+                contador++;
+            }
+          Console.WriteLine("fim da linha");
 
-            Console.WriteLine(" A opção selecionada foi " + OpcaoSelecionada );
-            
-       
+
+
+
+
+
             Console.ReadLine();
         }
     }
